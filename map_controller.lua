19c90b1db7f8932dc6212d5e43436f33e1ae7a46
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2022-06-10 00:10:53 +0200

    add waypoint handling
    Click the screen to add waypoints. They are drawn in magenta (next
    segment) and white (all subsequent). Outputs are set for number
    of waypoints, coordinates of next waypoint, coordinates of last waypoint.

--- map_controller.lua
+++ map_controller.lua
@@ -1,9 +1,32 @@
 --- map controller: show a centered, north-up map with position arrow for the current craft, centered on GPS coordinates
---- TODO: store, display a waypoint list; emit next waypoint and final waypoint
+---
+--- Inputs:
+---   Numbers:
+---     1. gps x
+---     2. gps y
+---     3. touch1 x
+---     4. touch1 y
+---     5. compass
+---   Bools:
+---     1. touch1 pressed
+---     2. clear waypoints
+---
+--- Outputs:
+---   Numbers:
+---     1. number of waypoints
+---     2. next waypoint x
+---     3. next waypoint y
+---     4. final waypoint x
+---     5. final waypoint y
+
 
 --- character dimensions
 CH = 5
 CW = 5
+
+--- screen dimensions
+W = 1
+H = 1
 
 --- current zoom level
 zoom = 1
@@ -16,6 +39,54 @@ lowY = 99999
 
 --- whether or not the screen is currently being touched
 touching = false
+
+--- queue impl; see https://www.lua.org/pil/11.4.html
+List = {}
+function List.new ()
+	return {first = 0, last = -1}
+end
+
+function List.pushright (list, value)
+	local last = list.last + 1
+	list.last = last
+	list[last] = value
+end
+
+function List.popleft (list)
+	local first = list.first
+	if first > list.last then return nil end
+	local value = list[first]
+	list[first] = nil        -- to allow garbage collection
+	list.first = first + 1
+	return value
+end
+
+function List.peekleft (list)
+	if list.first > list.last then return nil end
+	return list[list.first]
+end
+
+function List.peekright (list)
+	if list.first > list.last then return nil end
+	return list[list.last]
+end
+
+function List.window2 (list, callback)
+	local left = list.first
+	local right = left + 1
+	while right <= list.last do
+		callback(list[left], list[right])
+		left = right
+		right = right + 1
+	end
+end
+
+function List.len(list)
+	return list.last - list.first + 1
+end
+
+--- list of waypoints
+waypoints = List.new()
 
 --- constrain x to be between low and high
 function clamp(x, low, high)
@@ -59,6 +130,7 @@ function onTick()
 	touching = input.getBool(1)
 
 	handleZoom()
+	handleWaypoints()
 end
 
 function rotatePoint(x, y, degrees)
@@ -68,6 +140,47 @@ function rotatePoint(x, y, degrees)
 	local xp = x * cost - y * sint
 	local yp = y * cost + x * sint
 	return xp, yp
+end
+
+function handleZoom()
+	if touchX ~= nil and touchY ~= nil then
+		if touchY >= lowY then
+			if touchX >= centerX - CW and touchX < centerX then
+				zoom = clamp(zoom * zoomFactor, minZoom, maxZoom)
+				nt()
+			elseif touchX > centerX and touchX <= centerX + CW then
+				zoom = clamp(zoom / zoomFactor, minZoom, maxZoom)
+				nt()
+			end
+		end
+	end
+end
+
+function handleWaypoints()
+	if input.getBool(2) then --- clear waypoints list
+		waypoints = List.new()
+		for o=2,5 do output.setNumber(o, 0) end
+	end
+
+	if touchX ~= nil and touchY ~= nil then
+		if touchY < lowY or touchX < centerX - CW or touchX > centerX + CW	then
+			local worldx, worldy = map.screenToMap(gpsx, gpsy, zoom, W, H, touchX, touchY)
+			List.pushright(waypoints, {x = worldx, y = worldy})
+			nt()
+		end
+	end
+
+	output.setNumber(1, List.len(waypoints))
+	local nxt = List.peekleft(waypoints)
+	if nxt ~= nil then
+		output.setNumber(2, nxt.x)
+		output.setNumber(3, nxt.y)
+	end
+	local last = List.peekright(waypoints)
+	if last ~= nil then
+		output.setNumber(4, last.x)
+		output.setNumber(5, last.y)
+	end
 end
 
 function drawSelfArrow()
@@ -93,22 +206,8 @@ function drawSelfArrow()
 	end
 end
 
-function handleZoom()
-	if touchX ~= nil and touchY ~= nil then
-		if touchY >= lowY then
-			if touchX >= centerX - CW and touchX < centerX then
-				zoom = clamp(zoom * zoomFactor, minZoom, maxZoom)
-				nt()
-			elseif touchX >= centerX + 1 and touchX < centerX + 1 + CW then
-				zoom = clamp(zoom / zoomFactor, minZoom, maxZoom)
-				nt()
-			end
-		end
-	end
-end
-
 function drawZoom()
-	lowY = screen.getHeight() - CH
+	lowY = H - CH
 
 	screen.setColor(0, 0, 0, 200)
 	screen.drawRectF(centerX - CW, lowY, CW, CH)
@@ -119,12 +218,35 @@ function drawZoom()
 	screen.drawText(centerX + 2, lowY, "+")
 end
 
+function drawWaypoints()
+	if List.len(waypoints) > 0 then
+		--- line to next waypoint is magenta
+		local nxt = List.peekleft(waypoints)
+		local wptx, wpty = map.mapToScreen(gpsx, gpsy, zoom, W, H, nxt.x, nxt.y)
+
+		screen.setColor(69,0,48,175)
+		screen.drawLine(centerX, centerY, wptx, wpty)
+
+		--- line between subsequent waypoints is white
+		screen.setColor(96,96,96,175)
+		List.window2(waypoints, function(left, right)
+			lx, ly = map.mapToScreen(gpsx, gpsy, zoom, W, H, left.x, left.y)
+			rx, ry = map.mapToScreen(gpsx, gpsy, zoom, W, H, right.x, right.y)
+			screen.drawLine(lx, ly, rx, ry)
+		end)
+	end
+end
+
 function onDraw()
-	centerX = screen.getWidth() / 2
-	centerY = screen.getHeight() / 2
-	screen_dimension = math.min(screen.getWidth(), screen.getHeight())
+	W = screen.getWidth()
+	H = screen.getHeight()
+
+	centerX = W / 2
+	centerY = H / 2
+	screen_dimension = math.min(W, H)
 
     screen.drawMap(gpsx, gpsy, zoom)
+    drawWaypoints()
     drawSelfArrow()
     drawZoom()
 end
